d78e24d8a764a25ab7bd6fff70ca0caa8c64ec7e
tyreecepaul <89tyreecepaul@gmail.com>
2025-07-28 17:04:58 +1000

    Refactor code structure for improved readability and maintainability

--- dataset.py
+++ dataset.py
@@ -13,13 +13,14 @@ class BilingualDataset(Dataset):
         self.tokenizer_tgt = tokenizer_tgt
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
+        self.seq_len = seq_len
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(('[SOS]'))], dtype=torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(('[EOS]'))], dtype=torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(('[PAD]'))], dtype=torch.int64)
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id('[SOS]')], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id('[EOS]')], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id('[PAD]')], dtype=torch.int64)
 
     def __len__(self):
-        return len(self.ds)
+        return len(self.ds) # Bug
     
     def __getitem__(self, index: Any) -> Any:
         src_target_pair = self.ds[index]
@@ -35,7 +36,7 @@ class BilingualDataset(Dataset):
         if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
             raise ValueError("Sentence is too long")
         
-        encoder_input = torch.cut(
+        encoder_input = torch.cat(
             [
                 self.sos_token, 
                 torch.tensor(enc_input_tokens, dtype=torch.int64),
--- train.py
+++ train.py
@@ -26,26 +26,26 @@ def get_all_sentences(ds, lang):
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
     if not Path.exists(tokenizer_path):
-        toeknizer = Tokenizer(WordLevel(unk_token='[UNK]'))
-        toeknizer.pre_tokenizer = Whitespace()
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
-        toeknizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
-        toeknizer.save(str(tokenizer_path))
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
 
 def get_ds(config):
-    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["img-tgt"]}', split='train')
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['img_src'])
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
 
     train_ds_size = int(0.9 * len(ds_raw))
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_size, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
 
-    train_ds = BilingualDataset(train_ds_size, tokenizer_src, config["img_src"], config["lang_tgt"], config["seq_len"])
-    val_ds = BilingualDataset(val_ds_size, tokenizer_src, config["img_src"], config["lang_tgt"], config["seq_len"])
+    train_ds = BilingualDataset(train_ds_size, tokenizer_src, tokenizer_tgt, config["lang_src"], config["lang_tgt"], config["seq_len"])
+    val_ds = BilingualDataset(val_ds_size, tokenizer_src, tokenizer_tgt, config["lang_src"], config["lang_tgt"], config["seq_len"])
 
     max_len_src = 0
     max_len_tgt = 0
